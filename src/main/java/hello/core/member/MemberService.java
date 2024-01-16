b869c598335820ca685a663a2f99b89cceec8b91
kohuiseong <gmltjd904@gmail.com>
2024-01-16 14:37:36 +0900

    fix: 회원 가입 메서드 수정

--- src/main/java/hello/core/member/MemberService.java
+++ src/main/java/hello/core/member/MemberService.java
@@ -3,7 +3,7 @@ package hello.core.member;
 public interface MemberService {
 
     // 회원 가입
-    static void join(Member member);
+    void join(Member member);
 
     // 회원 조회
     Member findMember(Long memberId);
