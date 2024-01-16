024bb9848eb94a4214bdd892e3015c1839005240
kohuiseong <gmltjd904@gmail.com>
2024-01-16 15:14:01 +0900

    feat: 주문 할인 정책 테스트

---
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,20 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+
+    private int discountPercent = 10;
+
+    // command + shift + t => 테스트 만들기
+    // Testting library => Junit5 로 설정 하기
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
