cebc1d283b350748988a751f36f7d8980226a8fe
kohuiseong <gmltjd904@gmail.com>
2024-01-19 17:49:07 +0900

    fix: @Configuration, @Bean 등록

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,6 +8,8 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // 프로젝트의 전반적인 설정을 설정 하는 곳
 // AppConfig는 애플리케이션의 실제 동작에 필요한 구현 객체를 생성한다
@@ -19,24 +21,35 @@ import hello.core.order.OrderServiceImpl;
 // command + option + m
 // MemoryMemberRepository -> memberRepository로 변경
 // DisCount 메서드 만든 후 reuturn 값을 FixDiscountPolicy로 받기
+
+// Appconfig 스프링 기반으로 개발
+// @Bean이 등록 되면 자동으로 스프링 컨테이너로 등록 된다.
+// ApplicationContext를 스프링 컨터이너라고 한다
+// 스프링빈은  @Bean이 붙은 메서드명을 스프링 빈의 이름으로 사용 된다.
+// 스프링 컨테이너는 @Configuration이 붙은 AppConfig 를 설정 정보로 사용한다. 여기서 @Bean 이라 적힌 메서드를 모두 호출 해서 스프링 컨테이너에 등록
+// 기존에는 개발자가 직접 자바 코드로 모든 것을 했다면 이제부터는 스프링 컨테이너에 객체를 스프링 빈으로 등록하고 스프링 컨테이너에서 스프링 빈을 찾아서 사용 하도록 변경 했다
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository()); // 생성자 주입
         // 나는 MemberServiceImpl 인데 MemoryMemberRepository를 쓸꺼야
     }
 
-    private static MemoryMemberRepository memberRepository() {
+    @Bean
+   public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         // return new FixDiscountPolicy();
-
         // 구체화로 변경
         // FixDiscountPolicy -> RateDiscountPolicy로 변경
         return new RateDiscountPolicy();
