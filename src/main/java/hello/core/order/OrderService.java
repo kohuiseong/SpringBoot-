6762054fc086cf963e819bbb70522ccc599d45cf
kohuiseong <gmltjd904@gmail.com>
2024-01-26 16:07:41 +0900

    fix: import 수정

--- src/main/java/hello/core/order/OrderService.java
+++ src/main/java/hello/core/order/OrderService.java
@@ -1,7 +1,5 @@
 package hello.core.order;
 
-import org.springframework.core.annotation.Order;
-
 public interface OrderService {
     Order createOrder(Long memberId, String itemName, int itemPrice);
 }
